679d046fd39d1ecdbfe063dd8dd19bc8705a4dd8
Jaime-Felix <jaime.fm@culiacan.tecnm.mx>
2024-05-27 14:17:51 -0700

    Agregar archivos de proyecto.

---
+++ Examen-IRa/Examen-IRa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_IRa
+{
+    internal class Examen_IRa
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
